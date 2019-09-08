de158bb953574ecb81bb9b1792b5094124ebed26
Alex Clark <aclark.xyz@gmail.com>
2019-09-08 14:40:25 -0400

    Removed misc (again)

--- misc/testinchi.py
+++
@@ -1,9 +0,0 @@
-#fnord
-
-from ccdc.io import MoleculeReader
-rdr = MoleculeReader('CSD')
-from inchi import InChIGenerator
-gen = InChIGenerator()
-mol = rdr.molecule('ZIYZEP')
-inchi = gen.generate(rdr[0], include_stereo=True)
-print inchi.inchi
